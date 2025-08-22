740ee5e9c122590808cc65db5c55d29ca164020e
tais1719 <taissantos123098@gmail.com>
2025-08-22 16:26:41 -0300

    git commit -m "ajustes no OffersCarousel"

--- src/components/OffersCarousel/index.jsx
+++ src/components/OffersCarousel/index.jsx
@@ -1,71 +1,66 @@
 import { useEffect, useState } from 'react'
 import Carousel from 'react-multi-carousel'
 import 'react-multi-carousel/lib/styles.css'
-import { api }  from '../../services/api'
+import { api } from '../../services/api'
 
-
-import { Container, Title, } from './styles';
+import { Container, Title } from './styles';
 import { CardProduct } from '../CardProduct';
 import { FormatPrice } from '../../utils/formatPrice';
 
-
 export function OffersCarousel() { 
-    const [offers, setOffers] = useState([])
+  const [offers, setOffers] = useState([])
 
-    useEffect(()=>{
-        async function loadProducts() { 
-            const {data }  = await api.get('/products')
+  useEffect(() => {
+    async function loadProducts() { 
+      const { data } = await api.get('/products')
 
-           const onlyOffers = data.filter(product => product.offer)
-           .map((product)=>({ CurrencyValue: FormatPrice(product.price),
-            ...product,
-           }))
-        
+      const onlyOffers = data
+        .filter(product => product.offer)
+        .map(product => ({
+          CurrencyValue: FormatPrice(product.price),
+          ...product,
+        }))
 
-           setOffers(onlyOffers)
-       
- 
+      setOffers(onlyOffers)
+    }
 
-         }
-        loadProducts()
-     }, [])
+    loadProducts()
+  }, [])
 
-const responsive = {
-  superLargeDesktop: {
-    breakpoint: { max: 3000, min: 1024 },
-    items: 3,
-    slidesToSlide: 3 
-  },
-  tablet: {
-    breakpoint: { max: 1024, min: 464 },
-    items: 2,
-    slidesToSlide: 2 
-  },
-  mobile: {
-    breakpoint: { max: 464, min: 0 },
-    items: 1,
-    slidesToSlide: 1 
-  } 
-};
+  const responsive = {
+    superLargeDesktop: {
+      breakpoint: { max: 3000, min: 1024 },
+      items: 5,          // 5 cards visíveis
+      slidesToSlide: 5   // desliza 5 por vez
+    },
+    tablet: {
+      breakpoint: { max: 1024, min: 464 },
+      items: 3,          // 3 cards visíveis
+      slidesToSlide: 3
+    },
+    mobile: {
+      breakpoint: { max: 464, min: 0 },
+      items: 1,          // 1 card visível
+      slidesToSlide: 1
+    } 
+  };
 
-
-    return(
-        <Container>
-           <Title>Ofertas Exclusivas!</Title> 
-         
-          <Carousel
-    responsive={responsive}
-    infinite={true}
-    partialVisible={false}
-    itemClass="carousel-item"
-  >
-    {offers.map((product) => (
-      <CardProduct key={product.id} product= {product}/>
-   
-    ))}
- 
- 
+  return (
+    <Container>
+      <Title>Ofertas Exclusivas!</Title> 
+      
+      <Carousel
+        responsive={responsive}
+        infinite={true}
+        partialVisible={false}
+        itemClass="carousel-item"
+        containerClass="carousel-container"
+        arrows={true}
+      >
+        {offers.map(product => (
+          <CardProduct key={product.id} product={product}/>
+        ))}
       </Carousel>
-        </Container>
-    )
- }
+    </Container>
+  )
+}
