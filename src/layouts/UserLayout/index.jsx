207a72560afcca1b8918a9752d2ec3f04d901d84
tais1719 <taissantos123098@gmail.com>
2025-08-22 16:57:30 -0300

    git commit -m "Add UserLayout component with Header, Outlet and Footer"

--- src/layouts/UserLayout/index.jsx
+++ src/layouts/UserLayout/index.jsx
@@ -1,26 +1,22 @@
- import { Outlet } from "react-router-dom";
- import { Footer, Header } from "../../components";
+import { Outlet } from "react-router-dom";
+import { Footer, Header } from "../../components";
+import styled from "styled-components";
 
+const PageWrapper = styled.div`
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+`;
 
-
-
-
- 
-
-
- export function UserLayout(){ 
-
-    return(
-        <>
-        
-        
-        
-        <Header/>
-        <Outlet/>
-        <Footer/>
-        
-        
-        
-        </>
-    )
- }
+export function UserLayout() {
+  return (
+    <>
+      <Header />
+      <PageWrapper>
+        <Outlet />
+        <Footer />
+      </PageWrapper>
+    </>
+  );
+}
